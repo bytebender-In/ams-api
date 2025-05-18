b0b928087e629613790232f99dd1c86f476ab476
vartikanand <vartikkanand@gmail.com>
2025-05-18 17:44:35 +0530

    chore(docs): setup Swagger API documentation

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,10 @@
 import { Module } from '@nestjs/common';
-import { AuthModule } from './modules/auth/auth.module';
+import { AuthModule } from './modules/user-management/auth/auth.module';
 import { DatabaseModule } from './core/database/database.module';
 import { CommonModule } from './common/common.module';
-import { UserModule } from './modules/user/user.module';
+import { UserModule } from './modules/user-management/user/user.module';
 import { ConfigModule } from '@nestjs/config';
 import { UserManagementModule } from './modules/user-management/user-management.module';
-import { OrganizationModule } from './modules/organization/organization.module';
 
 @Module({
   imports: [
@@ -15,7 +14,6 @@ import { OrganizationModule } from './modules/organization/organization.module';
     CommonModule,
     UserModule,
     UserManagementModule,
-    OrganizationModule,
   ],
   controllers: [],
   providers: [],
