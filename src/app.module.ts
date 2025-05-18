236f0583c194f8c47bfa736bf6f2e64fd91cf260
vartikanand <vartikkanand@gmail.com>
2025-05-18 17:32:18 +0530

    feat(core/database): add centralized PrismaService and DatabaseService

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,8 @@
 import { Module } from '@nestjs/common';
 import { AuthModule } from './modules/auth/auth.module';
-import { DatabaseModule } from './database/database.module';
+import { DatabaseModule } from './core/database/database.module';
 import { CommonModule } from './common/common.module';
-import { UsersModule } from './modules/users/users.module';
+import { UserModule } from './modules/user/user.module';
 import { ConfigModule } from '@nestjs/config';
 
 @Module({
@@ -11,7 +11,7 @@ import { ConfigModule } from '@nestjs/config';
     AuthModule,
     DatabaseModule,
     CommonModule,
-    UsersModule,
+    UserModule,
   ],
   controllers: [],
   providers: [],
--- src/modules/user/user.module.ts
+++ src/modules/user/user.module.ts
@@ -1,8 +1,12 @@
 import { Module } from '@nestjs/common';
 import { RoleModule } from './role/role.module';
 import { PermissionsModule } from './permissions/permissions.module';
+import { UserController } from './user.controller';
+import { userService } from './user.service';
 
 @Module({
+  controllers:[UserController],
+  providers:[userService],
   imports: [RoleModule, PermissionsModule]
 })
-export class UsersModule {}
+export class UserModule {}
