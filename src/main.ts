d1b851ca47cd2a4e2fd202c885300dcc9866252f
vartikanand <vartikkanand@gmail.com>
2025-05-25 19:12:37 +0530

    fix: add startup log to verify deployment

--- src/main.ts
+++ src/main.ts
@@ -14,32 +14,21 @@ async function bootstrap() {
     .setTitle('AMS API')
     .setDescription('API documentation for Adaptive Management System')
     .setVersion('1.0')
-    .addTag(
-      'User Management',
-      // 'Endpoints for managing users, roles, permissions',
-    )
+    .addTag('User Management')
     .addBearerAuth()
     .build();
 
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('docs', app, document);
 
-  // Apply Prisma exception filter globally
   app.useGlobalFilters(new PrismaClientExceptionFilter());
-
-  // Use global validation pipe
   app.useGlobalPipes(new CustomValidationPipe());
   app.useGlobalFilters(new GlobalExceptionFilter());
   app.useGlobalInterceptors(new ResponseInterceptor());
-
-  // Logger
   app.useLogger(['debug', 'error', 'fatal', 'log', 'verbose', 'warn']);
 
-  app.setGlobalPrefix('v1');
-
-  await app.listen(process.env.PORT || 3000, '0.0.0.0');
-  // console.log(
-  //   `Application running at http://localhost:${process.env.PORT ?? 3000}/`,
-  // );
+  const port = process.env.PORT || 3000;
+  await app.listen(port, '0.0.0.0');
+  console.log(`🚀 App running on http://0.0.0.0:${port}`);
 }
 bootstrap();
